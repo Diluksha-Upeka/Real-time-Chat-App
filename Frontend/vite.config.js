6b00a859531c97c08b7f50d3cf21912c28f21168
Upeka Diluksha <dilukshaupeka@gmail.com>
2024-07-31 10:22:19 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,17 +3,13 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-      },
-    },
-  },
-  build: {
-    outDir: 'frontend/dist', // Set the output directory
-    emptyOutDir: true, // Optional: clear out the output directory before building
-  },
+	plugins: [react()],
+	server: {
+		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:5000",
+			},
+		},
+	},
 });
