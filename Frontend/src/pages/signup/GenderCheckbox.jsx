ddbab15c763fa27d9f83d05930160ffdf933a24d
Upeka Diluksha <dilukshaupeka@gmail.com>
2024-10-06 22:54:15 +0530

    evl

--- Frontend/src/pages/signup/GenderCheckbox.jsx
+++ Frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,31 +1,32 @@
 const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
-	return (
-	  <div className="flex space-x-6 mt-4">
-		<div className="form-control">
-		  <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "text-purple-600" : ""}`}>
-			<input
-			  type="checkbox"
-			  className="checkbox border-slate-900"
-			  checked={selectedGender === "male"}
-			  onChange={() => onCheckboxChange("male")}
-			/>
-			<span className="label-text">Male</span>
-		  </label>
-		</div>
-		<div className="form-control">
-		  <label className={`label gap-2 cursor-pointer ${selectedGender === "female" ? "text-purple-600" : ""}`}>
-			<input
-			  type="checkbox"
-			  className="checkbox border-slate-900"
-			  checked={selectedGender === "female"}
-			  onChange={() => onCheckboxChange("female")}
-			/>
-			<span className="label-text">Female</span>
-		  </label>
-		</div>
-	  </div>
-	);
-  };
-  
-  export default GenderCheckbox;
-  
+    return (
+        <div className="flex space-x-6 mt-4">
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "text-purple-600" : ""}`}>
+                    <input
+                        type="radio"
+                        className="radio border-slate-900"
+                        checked={selectedGender === "male"}
+                        onChange={() => onCheckboxChange("male")}
+                        name="gender" // Ensure radio buttons are grouped
+                    />
+                    <span className="label-text">Male</span>
+                </label>
+            </div>
+            <div className="form-control">
+                <label className={`label gap-2 cursor-pointer ${selectedGender === "female" ? "text-purple-600" : ""}`}>
+                    <input
+                        type="radio"
+                        className="radio border-slate-900"
+                        checked={selectedGender === "female"}
+                        onChange={() => onCheckboxChange("female")}
+                        name="gender" // Ensure radio buttons are grouped
+                    />
+                    <span className="label-text">Female</span>
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default GenderCheckbox;
